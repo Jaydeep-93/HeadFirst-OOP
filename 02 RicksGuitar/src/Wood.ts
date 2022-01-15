48e80806633837143e7bc98cb40a19862d1d28b0
Jay <jaydeepkachare123@gmail.com>
2022-01-15 19:15:02 +0530

    Enum added

---
+++ 02 RicksGuitar/src/Wood.ts
@@ -0,0 +1,13 @@
+enum Wood {
+    INDIAN_ROSEWOOD = "Indian Rosewood",
+    BRAZILIAN_ROSEWOOD = "Brazilian Rosewood",
+    MAHOGANY = "MAHOGANY",
+    MAPLE = "Maple",
+    COCOBOLO = "Cocobolo",
+    CEDAR = "Cedar",
+    ADIRONDACK = "Adriondack",
+    ALDER = "Alder",
+    SITKA = "Sitka" 
+}
+
+export { Wood }
